ad333b56b2291489b5d4f95b5e2afeb47a91aa78
vjjd <bibikabum@gmail.com>
2016-02-03 09:24:49 +0600

    Rscript on express.server

--- child_test.js
+++ child_test.js
@@ -1,16 +1,20 @@
 'use strict';
 
-const spawn = require('child_process').spawn;
-const ls = spawn('Rscript', ['stepic_w1.r']);
+function run () {
+    const spawn = require('child_process').spawn;
+    const r = spawn('Rscript', ['stepic_w1.r']);
 
-ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-});
+    r.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
 
-ls.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-});
+    r.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
 
-ls.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});
+    r.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
+
+module.exports.run = run;
--- server.js
+++ server.js
@@ -2,12 +2,11 @@
 
 let config = require('./config');
 let express = require('express');
-let r = require('./child_test.js');
+let rscript = require('./child_test');
+
 let app = express();
 
-app.get('/', (req, res) => {
-    res.render('index', {r: r.script()});
-});
+require('./lib/routes')(app, rscript);
 
 app.set('view engine', 'ejs');
 app.listen(config.port);
